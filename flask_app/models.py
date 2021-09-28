a020837d6674f0b856f940364d6e3215f688780a
Mohammed Hashim <mohammedhashim4498@gmail.com>
2021-09-28 20:14:06 +0200

    Added login_count to user model

--- flask_app/models.py
+++ flask_app/models.py
@@ -6,9 +6,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     username = db.Column(db.String(64), index=True, unique=True)
-    fullname = db.Column(db.String(100), index=True, unique=True)
+    fullname = db.Column(db.String(100), index=True, unique=False)
     email = db.Column(db.String(120), index=True, unique=True)
-
+    login_counts = db.Column(db.Integer,default=0)
     password_hash = db.Column(db.String(128))
 
     def set_password(self, password):
