5072c8838e5136dc2414804b925c0b1a211db3e2
Mohammed Hashim <mohammedhashim4498@gmail.com>
2021-09-29 13:19:22 +0200

    Hide password hash from admin panel

--- flask_app/admin.py
+++ flask_app/admin.py
@@ -2,9 +2,15 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from models import User
 
-from app import app,db
+from app import app, db
 
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
 admin = Admin(app)
-admin.add_view(ModelView(User, db.session))
+
+
+class UserAdminView(ModelView):
+    column_exclude_list = ['password_hash']
+
+
+admin.add_view(UserAdminView(User, db.session))
