73f40936c500e2b2140bb99d2a89c41763fe04db
batuogkoc <bgunduz21@ku.edu.tr>
2023-01-22 17:05:41 +0300

    added error message for when db_path.txt isn't found

--- flask_app/app.py
+++ flask_app/app.py
@@ -11,8 +11,14 @@ currentDirectory = os.getcwd()
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + databasePath
 databasePathFile = os.path.join(currentDirectory, "db_path.txt")
 databasePath = ""
-with open(databasePathFile, 'r') as f:
-    databasePath = f.readline().strip()
+try:
+    with open(databasePathFile, 'r') as f:
+        databasePath = f.readline().strip()
+except FileNotFoundError:
+    print(f"No file named {databasePathFile} exists.")
+    print("Create the file and write the path to your db in the format below:")
+    print("mysql://username:password@localhost/db_name")
+    exit()
 print(databasePath)
 app.config['SQLALCHEMY_DATABASE_URI'] = databasePath
 db = SQLAlchemy(app)
